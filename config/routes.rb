b8a885ebb931e3321e49c33e78106ce367485fca
zhaozongchang <xingyingsuifeng@gmail.com>
2018-09-26 17:37:46 +0800

    users can see their orders

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,10 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :orders 
+  resources :orders
+
+  namespace :account do
+    resources :orders
+  end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
