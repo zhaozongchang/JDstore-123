f29f4b4ab85082c0ebe699557ee6d7586935ab48
zhaozongchang <xingyingsuifeng@gmail.com>
2018-09-28 15:33:26 +0800

    add ship/cancel/return action to admin/orders_controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,5 +11,23 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @product_lists = @order.product_lists
+  end
+
+  def ship
+    @order = Order.find(params[:id])
+    @order.ship!
+    redirect_to :back
+  end
+
+  def shipped
+    @order = Order.find(prams[:id])
+    @order.deliver!
+    redirect_to :back
+  end
+
+  def return
+    @order = Order.find(params[:id])
+    @order.return_good!
+    redirect_to :back
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,14 @@ Rails.application.routes.draw do
   end
 
   namespace :account do
-    resources :orders
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
