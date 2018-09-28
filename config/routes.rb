1e2339dfc3d1ccf5ace07f552a2ea46bfb7eba8b
zhaozongchang <xingyingsuifeng@gmail.com>
2018-09-28 09:58:18 +0800

    add admin/orders

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :products
+    resources :orders
   end
 
   resources :products do
@@ -19,10 +20,15 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
 
   namespace :account do
     resources :orders
-  end 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
