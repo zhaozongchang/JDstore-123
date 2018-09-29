17572d2251ed3df9e5d57178cd636eb36f83b2bc
zhaozongchang <xingyingsuifeng@gmail.com>
2018-09-29 08:59:38 +0800

    add apply_cancel function

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -20,8 +20,14 @@ class Admin::OrdersController < ApplicationController
   end
 
   def shipped
-    @order = Order.find(prams[:id])
+    @order = Order.find(params[:id])
     @order.deliver!
+    redirect_to :back
+  end
+
+  def cancel
+    @order = Order.find(params[:id])
+    @order.cancel_order!
     redirect_to :back
   end
 
@@ -29,5 +35,5 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order.return_good!
     redirect_to :back
-  end 
+  end
 end
